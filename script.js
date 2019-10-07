ab4b59396f35cd399aba5d4f240390850161070f
Orlando Arnosa <oarnosa@gmail.com>
2019-10-07 00:07:25 -0400

    Add real time chat support

---
+++ script.js
@@ -0,0 +1,43 @@
+// location of socket.io
+const socket = io('http://localhost:3000');
+
+// get html elements
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+
+// ask for user name
+const name = prompt('What is your name?');
+appendMessage(`Hey ${name}, Welcome to Chatter!`);
+socket.emit('new-user', name);
+
+// show chat message
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`);
+});
+
+// show if user is connected
+socket.on('user-connected', name => {
+  appendMessage(`${name} has entered the chatroom!`);
+});
+
+// show if user is disconnected
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} has left the chat room`);
+});
+
+// send meesage typed by the user
+messageForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`);
+  socket.emit('send-chat-message', message);
+  messageInput.value = '';
+});
+
+// display message in new div
+function appendMessage(message) {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+// require socket.io on given port
+const io = require('socket.io')(3000);
+
+// list of users
+const users = {};
+
+// on connection successful
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  });
+  // on message sent
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', {
+      message: message,
+      name: users[socket.id]
+    });
+  });
+  // on user disconenct
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  });
+});
