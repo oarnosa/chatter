acb9eab09fbeeea1ac5aef14f51053b144d5e069
Orlando Arnosa <oarnosa@gmail.com>
2019-10-08 23:42:11 -0400

    Fix chat styling, add message types

--- script.js
+++ script.js
@@ -8,36 +8,38 @@ const messageInput = document.getElementById('send-msg');
 
 // ask for user name
 const name = prompt('What is your name?');
-appendMessage(`Hey ${name}, Welcome to Chatter!`);
+appendMessage(`Hey ${name}, Welcome to Chatter!`, 'welcome');
 socket.emit('new-user', name);
 
 // show chat message
 socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`);
+  console.log(data);
+  appendMessage(`${data.name}: ${data.message}`, 'incoming');
 });
 
 // show if user is connected
 socket.on('user-connected', name => {
-  appendMessage(`${name} has entered the chatroom!`);
+  appendMessage(`${name} has entered the chatroom!`, 'connected');
 });
 
 // show if user is disconnected
 socket.on('user-disconnected', name => {
-  appendMessage(`${name} has left the chat room`);
+  appendMessage(`${name} has left the chat room`, 'disconnected');
 });
 
 // send message typed by the user
 messageForm.addEventListener('submit', e => {
   e.preventDefault();
   const message = messageInput.value;
-  appendMessage(`You: ${message}`);
+  appendMessage(`You: ${message}`, 'outgoing');
   socket.emit('send-chat-message', message);
   messageInput.value = '';
 });
 
 // display message in new div
-function appendMessage(message) {
+function appendMessage(message, type) {
   const messageElement = document.createElement('div');
+  messageElement.setAttribute('class', type);
   messageElement.innerText = message;
   messageContainer.append(messageElement);
 }
