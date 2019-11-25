9a598d50950a214f855b3abb185783d6d50af0ac
Orlando Arnosa <oarnosa@gmail.com>
2019-11-25 09:39:10 -0500

    Create server-side user helper functions

--- client/src/components/chat/chat.component.jsx
+++ client/src/components/chat/chat.component.jsx
@@ -18,7 +18,14 @@ const Chat = ({ location }) => {
 
     setName(name);
     setRoom(room);
-  });
+
+    socket.emit('join', { name, room });
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
 
   return (
     <div>
