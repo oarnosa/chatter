cbf30f6d7212b4da273174466e4ed3e782d95088
Orlando Arnosa <oarnosa@gmail.com>
2019-11-26 09:44:07 -0500

    Add message input and broadcast handlers

--- server/index.js
+++ server/index.js
@@ -16,9 +16,7 @@ io.on('connection', socket => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if (error) {
-      return callback(error);
-    }
+    if (error) return callback(error);
 
     socket.emit('message', {
       user: 'admin',
@@ -29,6 +27,14 @@ io.on('connection', socket => {
       .emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
 
     callback();
   });
