91cc9db0b6c87862eb3e1f07eaf8e165de3c6630
Orlando Arnosa <oarnosa@gmail.com>
2020-01-16 16:42:17 -0500

    Add theme variables, slight modifications to UI

--- client/src/components/join/join.component.jsx
+++ client/src/components/join/join.component.jsx
@@ -8,22 +8,22 @@ const Join = () => {
   const [room, setRoom] = useState('');
 
   return (
-    <div className='join-outer-container'>
-      <div className='join-inner-container'>
-        <h1 className='heading'>Chatter</h1>
+    <div className="join-outer-container">
+      <div className="join-inner-container">
+        <h1 className="heading">Chatter</h1>
         <div>
           <input
-            type='text'
-            className='join-input'
-            placeholder='Name'
+            type="text"
+            className="join-input"
+            placeholder="Name"
             onChange={event => setName(event.target.value)}
           />
         </div>
         <div>
           <input
-            type='text'
-            className='join-input mt-20'
-            placeholder='Room'
+            type="text"
+            className="join-input mt-20"
+            placeholder="Room"
             onChange={event => setRoom(event.target.value)}
           />
         </div>
@@ -31,8 +31,8 @@ const Join = () => {
           onClick={event => (!name || !room ? event.preventDefault() : null)}
           to={`/chat?name=${name}&room=${room}`}
         >
-          <button className='button mt-20' type='submit'>
-            Join the Conversation
+          <button className="button mt-20" type="submit">
+            <strong>Join the Conversation</strong>
           </button>
         </Link>
       </div>
